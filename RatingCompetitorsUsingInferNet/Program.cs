4c56b89c59212e3de9637d5e62a4155d55372dbc
Oleksii Prosiankin <Oleksii.Prosiankin@gov.bc.ca>
2019-10-04 14:13:54 -0700

    Add project files.

---
+++ RatingCompetitorsUsingInferNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RatingCompetitorsUsingInferNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
